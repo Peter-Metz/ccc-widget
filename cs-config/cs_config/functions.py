6648d95b13802615b99d42916266624336fcb8bf
Henry "Hank" Doupe <henrymdoupe@gmail.com>
2020-09-05 10:51:20 -0400

    Update functions.py

--- cs-config/cs_config/functions.py
+++ cs-config/cs_config/functions.py
@@ -4,24 +4,6 @@ from app import server
 
 
 def get_version():
-    return "hank test"
-
-
-def get_inputs(meta_param_dict):
-    pass
-
-
-def validate_inputs(meta_param_dict, adjustment, errors_warnings):
-    pass
-
-
-def run_model(meta_param_dict, adjustment):
-    pass
-
-
-# def dash():
-#     app.run_server(
-#         port=os.environ.get("PORT", 8050),
-#     )
+    return "version here"
 
 dash = server
